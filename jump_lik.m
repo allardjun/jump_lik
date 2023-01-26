9cbea6afa6ccd4761bb0e4cd699903ef0bb392a6
Jun Allard <jun.allard@uci.edu>
2023-01-26 12:16:49 -0800

    nonuniformity bug fixed - normalized!

--- jump_lik.m
+++ jump_lik.m
@@ -2,13 +2,12 @@
 % of different diffusion coefficients and elastic forces
 % Sohyeon Park, Jun Allard, allardlab.com
 
+verbose = 1; % set to 1 for debugging
 
+% incoming parameters
 dt_obs = 0.05; % time between observations
 
-dz_numerical = 0.001; %numerical spatial step -- This controls accuracy.
-
 z_max = 2; % domain size
-
 
 xi = 1; % viscosity, pNs/um
 kBT = 4.2e-3; % pNum
@@ -18,6 +17,9 @@ width_TZ = 0.1; % width of tz
 
 xi_TZ=2*xi;
 
+% internal parameters
+dz_numerical = 0.001; %numerical spatial step -- This controls accuracy.
+
 % -- consequential parameters
 
 D = kBT/xi;
@@ -25,15 +27,17 @@ v = UTZ/(width_TZ*xi_TZ);
 
 D_TZ = kBT/xi_TZ;
 
-Peclet = (v*dz_numerical)/D;
 
 dt_numerical_estimate = min( [dz_numerical^2/D, dz_numerical/v] );
 dt_numerical = 16*dt_numerical_estimate;
 
-DZR = v*sqrt(dt_numerical/2*D);
-
-display(DZR)
-display(Peclet)
+if (verbose)
+    Peclet = (v*dz_numerical)/D;
+    DZR = v*sqrt(dt_numerical/2*D);
+    
+    display(DZR)
+    display(Peclet)
+end
 
 ntmax = round(dt_obs/dt_numerical);
 
@@ -47,33 +51,35 @@ n_up    = ceil( (z_TZ)/dz_numerical+2 ):ceil( (z_TZ+(width_TZ/2))/dz_numerical )
 
 
 % Diffusion matrix
-
+% NOTE Is this the Ito or Stratanovich diffusion matrix?
 D_array = D*ones(nz,1);
 D_array(n_TZ) = D_TZ;
 
-D_matrix = 1/(dz_numerical^2)*( -2*diag(D_array) + diag(D_array(2:end),-1)+ diag(D_array(1:end-1),+1) );
+D_matrix = 1/(dz_numerical^2)*( -2*diag(D_array) + diag(D_array(2:end),+1)+ diag(D_array(1:end-1),-1) );
 
 % periodic BCs for convenience
 D_matrix(1,end) = -sum(D_matrix(:,end));
-D_matrix(end,1) = -sum(D_matrix(:,1));
+D_matrix(end, 1) = -sum(D_matrix(:,1));
 
-sum(D_matrix)
+if verbose
+    figure(61); clf;
+    spy(D_matrix);
+    any(sum(D_matrix))   
+end
 
 % Advection matrix
 v_matrix = zeros(nz);
-v_matrix(n_down,n_down) = v/dz_numerical*( -1*diag(ones(numel(n_down),1)) + diag(ones(numel(n_down)-1,1),-1) );
-v_matrix(n_up,n_up)     = v/dz_numerical*( -1*diag(ones(numel(n_up),1))   + diag(ones(numel(n_up)-1,1),  +1) );
+v_matrix(n_down,n_down) = v/dz_numerical*( -1*diag(ones(numel(n_down),1),0) + diag(ones(numel(n_down)-1,1),+1) );
+v_matrix(n_up,n_up)     = v/dz_numerical*( -1*diag(ones(numel(n_up),1)  ,0) + diag(ones(numel(n_up)-1,1),  -1) );
 
-%v_matrix(min(n_down)-1,min(n_down)) = + v/dz_numerical;
-%v_matrix(max(n_up)+1,max(n_up)) = + v/dz_numerical;
+v_matrix(min(n_down)-1,min(n_down)) = + v/dz_numerical;
+v_matrix(max(n_up)+1,max(n_up)) = + v/dz_numerical;
 
-
-% periodic BCs for convenience
-v_matrix(1,end) = -sum(v_matrix(:,end));
-v_matrix(end,1) = -sum(v_matrix(:,1));
-
-sum(v_matrix)
-
+if verbose
+    figure(62); clf;
+    spy(v_matrix);
+    any(sum(v_matrix))   
+end
 
 % invert!
 
@@ -86,7 +92,6 @@ pz_final = mpower(Back_matrix,ntmax);
 toc;
 
 
-% check for unity
 %sum(pz_final*dz_numerical)
 
 % Solver loop
@@ -101,6 +106,12 @@ end
 
 pz_final= pz_final/dz_numerical;
 
+if verbose
+    % check for unity
+    sum(pz_final*dz_numerical);
+    normalization_error = sum(pz_final*dz_numerical) - 1;
+    display(max(abs(normalization_error)));
+end
 
 %% analyze
 
@@ -108,7 +119,7 @@ pz_final= pz_final/dz_numerical;
 
 figure(1); clf; hold on; box on;
 
-plot(z_array,pz_final(1:100:end,:)')
+plot(z_array,pz_final(:,1:100:end))
 %set(gca,'ylim', [0,0.05])
 
 sigma_t = sqrt(2*D*dt_obs);
